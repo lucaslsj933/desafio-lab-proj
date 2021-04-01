112cbfe8d0e9e512dcada6c2bb295a096eab6f1b
lucaslsj933 <81392739+lucaslsj933@users.noreply.github.com>
2021-04-01 15:39:56 -0300

    0.4
    Uso de Batalha como static e melhora na readability

--- desafio_lab_proj/src/main/java/com/lucaslsj933/desafio_lab_proj/Batalha.java
+++ desafio_lab_proj/src/main/java/com/lucaslsj933/desafio_lab_proj/Batalha.java
@@ -11,17 +11,18 @@ import java.util.Scanner;
  * @author Lucas Non-Admin
  */
 public class Batalha {
-    Personagem[] pers=new Personagem[2];;
-    String local;
-    Scanner scanObj=new Scanner(System.in);
-    int persAtual;
-    public Batalha(String localParam,Personagem pers1,Personagem pers2) {
+    static Personagem[] pers=new Personagem[2];;
+    static String local;
+    static Scanner scanObj=new Scanner(System.in);
+    static int persAtual;
+    
+    public static void batalhaInit(String localParam,Personagem pers1,Personagem pers2) {
         local=localParam;
         pers[0]=pers1;
         pers[1]=pers2;
     }
     
-    public void batalhaMain(){
+    public static void batalhaMain(){
         char comando;
         persAtual=0;
         int danoAtual=0;
@@ -36,12 +37,12 @@ public class Batalha {
                 System.out.println("Digite o comando: (A para Atacar ou D para Defender): ");
                 comando=scanObj.next().charAt(0);
                 if((comando=='a')||(comando=='A')) {
-                    atacar(1);
+                    pers[persAtual].atacar(1);
                 }
             }
             else if(persAtual==1) {
                 System.out.println("\nTurno de "+pers[persAtual].nome+"\n");
-                atacar(0);
+                pers[persAtual].atacar(0);
             }
             //Mudança de turno
             if(persAtual==0)
@@ -49,28 +50,5 @@ public class Batalha {
             else if(persAtual==1)
                 persAtual=0;
         }
-    }
-    
-    public void atacar(int persInimId){
-        int danoAtual;
-        String strPrintDano="";
-        danoAtual=pers[persAtual].getDanoDoInim(pers[persInimId]);
-        setDanoPers(danoAtual);
-        if(pers[persAtual].sorte<=15)
-            strPrintDano="Errou - "+danoAtual+" "+"HP";
-        else if((pers[persAtual].sorte>=16)&&(pers[persAtual].sorte<=70))
-            strPrintDano="Normal - "+danoAtual+" "+"HP";
-        else if((pers[persAtual].sorte>=71)&&(pers[persAtual].sorte<=95))
-            strPrintDano="Sorte!!! - "+danoAtual+" "+"HP";
-        else if((pers[persAtual].sorte>=97)&&(pers[persAtual].sorte<=100))
-            strPrintDano="Crítico!!! - "+danoAtual+" "+"HP";
-        System.out.println(strPrintDano);
-    }
-    
-    public void setDanoPers(int danoParam) {
-        if(persAtual==0)
-            pers[1].energia=pers[1].energia-danoParam;
-        else if(persAtual==1)
-            pers[0].energia=pers[0].energia-danoParam;
     }
 }
--- desafio_lab_proj/src/main/java/com/lucaslsj933/desafio_lab_proj/Personagem.java
+++ desafio_lab_proj/src/main/java/com/lucaslsj933/desafio_lab_proj/Personagem.java
@@ -12,7 +12,7 @@ package com.lucaslsj933.desafio_lab_proj;
 public class Personagem {
     String nome;
     public int energia;
-    public int poder;
+    public float poder;
     public int poderMax;
     public int energiaMax;
     public int sorte;
@@ -38,17 +38,38 @@ public class Personagem {
         if(sorte<=15)
             dano=0;
         else if((sorte>=16)&&(sorte<=70))
-            dano=(float)poder*(1.0f/3.0f);
+            dano=poder*(1.0f/3.0f);
         else if((sorte>=71)&&(sorte<=95)) {
             //Ataque com sorte
-            temp=(float)poder*(1.0f/3.0f);
+            temp=poder*(1.0f/3.0f);
             dano=(int)(temp+temp*0.2f);
         }
         else if((sorte>=97)&&(sorte<=100)) {
             //Ataque crítico
-            temp=(float)poder*(1.0f/3.0f);
+            temp=poder*(1.0f/3.0f);
             dano=(int)temp*2;
         }
         return (int)dano;
+    }
+    
+    public void atacar(int persInimId){
+        int dano;
+        String strPrintDano="";
+        dano=getDanoDoInim(Batalha.pers[persInimId]);
+        Batalha.pers[persInimId].energia=Batalha.pers[persInimId].energia-dano;
+        printDanoDoInim(dano);
+    }
+    
+    public void printDanoDoInim(int danoParam) {
+        String strPrintDano="";
+        if(sorte<=15)
+            strPrintDano="Errou - "+danoParam+" "+"HP";
+        else if((sorte>=16)&&(sorte<=70))
+            strPrintDano="Normal - "+danoParam+" "+"HP";
+        else if((sorte>=71)&&(sorte<=95))
+            strPrintDano="Sorte!!! - "+danoParam+" "+"HP";
+        else if((sorte>=97)&&(sorte<=100))
+            strPrintDano="Crítico!!! - "+danoParam+" "+"HP";
+        System.out.println(strPrintDano);
     }
 }
